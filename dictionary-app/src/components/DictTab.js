000b0863278a2e316ec7cf37a3c668e4b43089ce
Max Moir <61817442+Macsmoya@users.noreply.github.com>
2022-05-30 21:09:13 +1200

    Started admin panel

--- dictionary-app/src/components/DictTab.js
+++ dictionary-app/src/components/DictTab.js
@@ -1,4 +1,4 @@
-import { Tab, Tabs, Card, Elevation } from "@blueprintjs/core";
+import { Tab, Tabs, Card, Elevation, Button, Intent, Classes, H5, PopoverInteractionKind, Position, Popover } from "@blueprintjs/core";
 import React from 'react';
 
 function DictTab(props){
@@ -41,10 +41,10 @@ function adminPanel(props){
     <div  style={{textAlign: "left"}}>
       <h1>Manage Categories</h1>
       <div className="bp4-card">
-        <h5>Delete category</h5>
+        <h3>Delete category</h3>
         <div className="bp4-html-select">
           <select className=".bp4-fill">
-            <option selected>Choose an item...</option>
+            <option selected>Choose a Category</option>
             {props.map(cat => (
             <option value={cat[0]}>{cat[1]}</option>
 
@@ -52,13 +52,27 @@ function adminPanel(props){
           </select>
           <span class="bp4-icon bp4-icon-double-caret-vertical"></span>
           </div> 
-          <button onclick="alert('test')">Try it - click</button>     
+          <br></br>
+          <br></br>
+          <Popover
+                interactionKind={PopoverInteractionKind.CLICK}
+                popoverClassName="bp4-popover-content-sizing"
+                position={Position.RIGHT}
+            >
+              <Button>Delete</Button>
+              <div>
+                  <h4>Are you sure you want to delete this category?</h4>
+                  <Button className="bp4-minimal" intent={Intent.DANGER} icon="delete" text="Delete" />
+
+              </div>
+          </Popover>
+        
       </div>
     </div>   
   )
 }
 function deleteCategory() {
-  alert("hey")
+  
 }
 
 function myTabs(props){
